ec3e39f7fa05d47df95cc2b04a15b250cd8a4bb0
vane11ope <vane11opeschw33tz@gmail.com>
2020-03-28 00:00:55 +0900

    A little bit tweak for the colors

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `ヴァネロピのポートフォリオっ(๑>◡<๑)`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Just a portfolio of Vane11ope`,
+    author: `Vane11ope`,
   },
   plugins: [
     `gatsby-plugin-typescript`,
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `rgb(153, 61, 6)`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: 'rgb(249, 239, 232)',
             textDecoration: `none`,
           }}
         >
