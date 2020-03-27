5bbbfc3d0dd6cbe20171bc33891863722387650f
vane11ope <vane11opeschw33tz@gmail.com>
2020-03-28 05:03:15 +0900

    Bunch of changes such as bg, headers, etc etc

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,32 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Image from "./image"
+import styled, { keyframes } from "styled-components"
+
+const FirstWave = keyframes`
+    0% { transform: rotate(0deg); }
+    40% { transform: rotate(0deg); }
+    50% { transform: rotate(25deg); }
+    60% { transform: rotate(-25deg); }
+    70% { transform: rotate(0deg); }
+`;
+
+const SecondWave = keyframes`
+    0% { transform: rotate(0deg); }
+    65% { transform: rotate(0deg); }
+    75% { transform: rotate(10deg); }
+    85% { transform: rotate(-10deg); }
+    95% { transform: rotate(0deg); }
+`;
+
+const Vane11opeStyles = styled.div` 
+  animation: ${FirstWave} 3s linear infinite;
+`;
+
+const TitleStyles = styled.div` 
+  animation: ${SecondWave} 3s linear infinite;
+`;
 
 const Header = ({ siteTitle }) => (
   <header
@@ -16,16 +42,19 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'rgb(249, 239, 232)',
-            textDecoration: `none`,
-          }}
-        >
+      <h1 style={{ 
+        margin: 0,
+        color: 'rgb(249, 239, 232)',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+      }}>
+        <Vane11opeStyles>
+          <Image />
+        </ Vane11opeStyles>
+        <TitleStyles>
           {siteTitle}
-        </Link>
+        </TitleStyles>
       </h1>
     </div>
   </header>
